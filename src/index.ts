90e0be343518c18822340e8d2c1511556d9672c3
André de Albuquerque <itstheandre@gmail.com>
2021-11-07 13:15:51 +0100

    fix(remove index file running immediately"): delete unnecesasry function call in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
 export * from "./main";
-import { copyTemplate } from "./main";
-import * as path from "path";
-
-(async () => {
-  copyTemplate({
-    inDir: path.join(__dirname, "..", "template", "newTmp"),
-    outDir: path.join(__dirname, "..", "newTmp"),
-    vars: { hello: "andre" },
-  });
-})();
